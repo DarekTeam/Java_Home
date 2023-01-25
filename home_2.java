4d9f52d75a9cff1baf7fd8ab4c19649394bea70d
DarekTim <zh.zh.1989@yandwx.ru>
2023-01-25 12:36:36 +0500

    two

---
+++ home_2.java
@@ -0,0 +1,7 @@
+public class home_2 {
+ // 2 Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+ public static int seniorBit(int i) {
+    int binLen = Integer.toBinaryString(i).length();
+    return binLen;  
+}
+}
